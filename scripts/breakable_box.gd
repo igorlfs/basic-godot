fc18fd4794e9cd9f5cbebbed925485772d81b563
Igor <igorlfs@ufmg.br>
2023-09-17 23:48:19 -0300

    part14: spawn coin when hitting boxes

--- scripts/breakable_box.gd
+++ scripts/breakable_box.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
 const BOX_PIECES = preload("res://prefabs/box_pieces.tscn")
+const COIN_INSTANCE = preload("res://prefabs/coin_rigid.tscn")
 const IMPULSE := 50
 
 @export var pieces: PackedStringArray
 @export var hit_points := 3
 
 @onready var animation_player := $Animation as AnimationPlayer
+@onready var spawn_coin = $Spawn_Coin as Marker2D
 
 
 func break_sprite():
@@ -19,3 +21,10 @@ func break_sprite():
 			Vector2(randi_range(-IMPULSE, IMPULSE), randi_range(2 * -IMPULSE, -IMPULSE))
 		)
 	queue_free()
+
+
+func create_coin():
+	var coin = COIN_INSTANCE.instantiate()
+	get_parent().call_deferred("add_child", coin)
+	coin.global_position = spawn_coin.global_position
+	coin.apply_impulse(Vector2(randi_range(-50, 50), -150))
--- scripts/player.gd
+++ scripts/player.gd
@@ -101,7 +101,8 @@ func _set_state():
 func _on_head_collider_body_entered(body: Node2D) -> void:
 	if body.has_method("break_sprite"):
 		body.hit_points -= 1
-		if body.hit_points < 1:
+		if body.hit_points < 0:
 			body.break_sprite()
 		else:
 			body.animation_player.play("hit")
+			body.create_coin()
