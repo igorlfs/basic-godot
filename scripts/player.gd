6c473db11b2a7850d3a0a3dea640bed07709c9f8
Igor <igorlfs@ufmg.br>
2023-09-29 18:23:45 -0300

    part17: spikes

--- scripts/player.gd
+++ scripts/player.gd
@@ -55,9 +55,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_hurtbox_body_entered(_body: Node2D) -> void:
-	if player_life < 0:
-		queue_free()
-	elif ray_right.is_colliding():
+	if ray_right.is_colliding():
 		take_damage(Vector2(-200, -200))
 	elif ray_left.is_colliding():
 		take_damage(Vector2(200, -200))
@@ -69,6 +67,11 @@ func follow_camera(camera: Camera2D) -> void:
 
 func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 	player_life -= 1
+
+	if player_life > 0:
+		player_life -= 1
+	else:
+		queue_free()
 
 	if knockback_force != Vector2.ZERO:
 		knockback_vector = knockback_force
