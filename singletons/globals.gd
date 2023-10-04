e252a89309e5c76ce229345606681843207a2a63
Igor <igorlfs@ufmg.br>
2023-10-04 00:28:19 -0300

    part22: checkpoint

--- singletons/globals.gd
+++ singletons/globals.gd
@@ -5,3 +5,11 @@ const NUM_PLAYER_LIVES := 3
 var coins := 0
 var score := 0
 var player_life := NUM_PLAYER_LIVES
+
+var player = null
+var current_checkpoint = null
+
+
+func respawn_player():
+	if current_checkpoint != null:
+		player.position = current_checkpoint.global_position
